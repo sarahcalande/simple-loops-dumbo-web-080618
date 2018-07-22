879f0fe5a75a5302cf6d493aad3f383d7f982f8c
scalande@yahoo.com <scalande@yahoo.com>
2018-07-22 12:52:17 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -40,9 +40,9 @@ def until_iterator(number_of_times)
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
 count = 0
-+  until count == number_of_times do
-+    puts phrase
-+    count += 1
+  until count == number_of_times do
+    puts phrase
+    count += 1
 end
 
 def for_iterator(number_of_times)
